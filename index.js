95ab1d51ed5ec79a05553df1330588dfcb712ec0
Anshuman <sanshuman841@gmail.com>
2026-09-08 23:21:27 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const htmlPath = path.join(__dirname, './public/index.html');
+const cssPath = path.join(__dirname, './public/style.css');
+const dataPath = path.join(__dirname, 'data.json'); 
+
+function readData(){
+    const data = fs.readFileSync(dataPath, 'utf-8');
+    const jParse = JSON.parse(data);
+
+    return jParse;
+}
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url
+
+  if (pathName === "/") {
+    const html = fs.readFileSync(htmlPath, 'utf-8');
+    res.writeHead(200, {'Content-type' : 'text/html'});
+    res.end(html);
+  } else if (pathName === "/style.css") {
+    const css = fs.readFileSync(cssPath, 'utf-8');
+    res.writeHead(200, {'Content-type' : 'text/css'});
+    res.end(css);
+  } else if(pathName==='/api/resources'){
+    const data = readData();
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    });
+    res.end(JSON.stringify(data)); //always expects a string buffer.
+
+  } else if(pathName === '/WebDev'){
+    //leaving this for now.
+  }
+  
+  else {
+    res.writeHead(404, {'Content-type' : 'html/text'});
+    res.end('<h1>404 Not Found</h1>');
+  }
+});
+
+
+
+
+
+const PORT = 8001;
+server.listen(PORT, ()=>{
+console.log(`listening at PORT ${PORT}`);
+});
+
+
+
+
---
+++ public/script.js
@@ -0,0 +1,34 @@
+async function loadResources(){
+    const response = await fetch('/api/resources');
+    const resources = await response.json();
+let count = 0;
+let countF = 0;
+function countTrue(){
+    for(let i = 0; i<resources.length; i++){
+        if(resources[i].completed === true){
+            count++;
+        }
+    }
+    return count;
+}
+
+function countFalse(){
+    for(let i = 0; i<resources.length; i++){
+        if(resources[i].completed === false){
+            countF++;
+        }
+    }
+    return countF;
+}
+
+    let allTotal = resources.length;
+    let allCompleted = countTrue();
+    let allPending = countFalse();
+}
+
+
+
+const totalResources = document.getElementById("totalResources");
+const completedResources = document.getElementById("completedResources");
+const pendingResources = document.getElementById("pendingResources");
+
